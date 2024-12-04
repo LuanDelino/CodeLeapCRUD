7e9b3c407b529281cac6de1e6338b1a0bf3d632c
Luan Delino <39099783+LuanDelino@users.noreply.github.com>
2024-12-04 12:58:48 -0300

    corrigir userpost model

--- app/api/models.py
+++ app/api/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 
 class UserPost(models.Model):
-    id = models.IntegerField()
     username = models.CharField(max_length=100)
     created_datetime = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=200)
